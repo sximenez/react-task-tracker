c6748f385b925dc60ab7bbaf2c993b4d10ecdfe3
sximenez <70152164+sximenez@users.noreply.github.com>
2022-12-12 16:35:12 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// import { Link } from "react-router-dom";
+
+const Footer = () => {
+
+  return (
+
+    <footer>
+
+      <p>Hello world!</p>
+      {/* <a href="/about">About</a> */}
+
+    </footer>
+
+  )
+
+}
+
+export default Footer;
